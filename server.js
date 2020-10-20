4d54b012744d69c72183ea1a4882e1e81de90714
mplowey28 <matthew.plowey@gmail.com>
2020-10-20 11:27:14 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const products = require('./data/products')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json'})
+    res.end(JSON.stringify(products))
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
