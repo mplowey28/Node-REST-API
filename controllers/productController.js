dc5353107f0e22dac357782a44ce92ae8ebd9a2d
mplowey28 <matthew.plowey@gmail.com>
2020-11-02 13:59:23 +0100

    added post reqest method

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,32 +1,49 @@
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 
 async function getProducts(req, res) {
-    try {
-        const products = await Product.findAll();
-        res.writeHead(200, { 'Content-Type': 'application/json'})
-        res.end(JSON.stringify(products))
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const products = await Product.findAll();
+		res.writeHead(200, { "Content-Type": "application/json" });
+		res.end(JSON.stringify(products));
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 async function getProduct(req, res, id) {
-    try {
-        const product = await Product.findById(id);
-        if(!product) {
-            res.writeHead(404, { 'Content-Type': 'application/json'})
-            res.end(JSON.stringify({ message: 'Product Not Found'}))
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json'})
-            res.end(JSON.stringify(product))
-        }
-        
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const product = await Product.findById(id);
+		if (!product) {
+			res.writeHead(404, { "Content-Type": "application/json" });
+			res.end(JSON.stringify({ message: "Product Not Found" }));
+		} else {
+			res.writeHead(200, { "Content-Type": "application/json" });
+			res.end(JSON.stringify(product));
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function createProduct(req, res) {
+	try {
+		const product = {
+			title: "Test Product",
+			description: "This is my product",
+			price: 100,
+		};
+
+		const newProduct = await Product.create(product);
+
+		res.writeHead(201, { "Content-Type": "application/json" });
+		return res.end(JSON.stringify(newProduct));
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 module.exports = {
-    getProducts,
-    getProduct
-}
+	getProducts,
+	getProduct,
+	createProduct,
+};
--- models/productModel.js
+++ models/productModel.js
@@ -1,20 +1,33 @@
-const products = require('../data/products');
+const products = require("../data/products");
+const { v4: uuidv4 } = require("uuid");
+
+const { writeDataToFile } = require("../utils");
 
 function findAll() {
-    return new Promise((resolve, reject) => {
-        resolve(products)
-    })
+	return new Promise((resolve, reject) => {
+		resolve(products);
+	});
 }
 
 function findById(id) {
-    return new Promise((resolve, reject) => {
-        const product = products.find((p) => p.id == id)
-        console.log(product)
-        resolve(product)
-    })
+	return new Promise((resolve, reject) => {
+		const product = products.find(p => p.id == id);
+		console.log(product);
+		resolve(product);
+	});
+}
+
+function create(product) {
+	return new Promise((resolve, reject) => {
+		const newProduct = { id: uuidv4(), ...product };
+		products.push(newProduct);
+		writeDataToFile("./data/products.json", products);
+		resolve(newProduct);
+	});
 }
 
 module.exports = {
-    findAll,
-    findById
-}
+	findAll,
+	findById,
+	create,
+};
