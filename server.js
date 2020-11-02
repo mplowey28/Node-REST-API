20857ffddfb1fe0ab2ade427e23937e6da207296
mplowey28 <matthew.plowey@gmail.com>
2020-11-02 13:41:38 +0100

    added post method

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const http = require('http');
-const { getProducts, getProduct } = require('./controllers/productController');
+const http = require("http");
+const {
+	getProducts,
+	getProduct,
+	createProduct,
+} = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/api/products' && req.method === 'GET') {
-        getProducts(req, res);
-    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
-        const id = req.url.split('/')[3]
-        getProduct(req, res, id)
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json'})
-        res.end(JSON.stringify({ message: 'Page not found' }))
-    }
-    
+	if (req.url === "/api/products" && req.method === "GET") {
+		getProducts(req, res);
+	} else if (
+		req.url.match(/\/api\/products\/([0-9]+)/) &&
+		req.method === "GET"
+	) {
+		const id = req.url.split("/")[3];
+		getProduct(req, res, id);
+	} else if (req.url === "/api/products" && req.method === "POST") {
+		createProduct(req, res);
+	} else {
+		res.writeHead(404, { "Content-Type": "application/json" });
+		res.end(JSON.stringify({ message: "Page not found" }));
+	}
 });
-
 
 const PORT = process.env.PORT || 5000;
 
